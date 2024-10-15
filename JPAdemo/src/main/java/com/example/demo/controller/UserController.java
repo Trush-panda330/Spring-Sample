ee3d284f4d1f21f0573f3eb7dacdbd3e96a7f5e4
unknown <uetak330@gmail.com>
2024-10-15 17:20:18 +0900

    cleanUp

--- JPAdemo/src/main/java/com/example/demo/controller/UserController.java
+++ JPAdemo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("user/list")
+	public String displayUserList(Model model) {
+		List<User> userList = userService.searchAll();
+		model.addAttribute("userList", userList);
+		return "user/list";
+		
+	}
 }
