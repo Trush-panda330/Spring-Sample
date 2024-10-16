7b4360808b59e8d97716b1c59628f227e21d1e92
uetak330 <uetak330@gmail.com>
2024-10-16 22:14:34 +0900

    Mapperファイルの定義

--- springSample/src/main/java/com/example/demo/dao/UserInfoMapper.java
+++ springSample/src/main/java/com/example/demo/dao/UserInfoMapper.java
@@ -9,50 +9,48 @@ import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.dto.UserUpdateRequest;
 import com.example.demo.entity.UserInfo;
 
+/**
+ * ユーザー情報 Mapper
+ */
 @Mapper
 public interface UserInfoMapper {
-	
-	/**
-	 * ユーザー情報全件検索
-	 * @param user 検索用リクエストデータ
-	 * @return 検索結果
-	 * */
-	List<UserInfo>  findAll();
-	
-	/**
-	 *ユーザー情報主キー検索
-	 *@param id 主キー
-	 *@return 検索結果
-	 * */
-	UserInfo findById(Long id);
-	
-	/**
-	 * ユーザー情報検索
-	 *  @param userSearchRequest
-	 *  @return 検索結果
-	 * */
-	List<UserInfo> search(UserSearchRequest user);
-	
-	/**
-	 * ユーザー情報登録
-	 * @param userCreateRequest 登録用リクエストデータ
-	 * */
-	void save(UserAddRequest userAddRequest);
-	
-	/**
-	 * ユーザー情報更新
-	 * @param userUpdateRequest
-	 * */
-	void update(UserUpdateRequest userUpdateRequest);
-	
-	
-	/**
-	 * ユーザー情報論理削除
-	 *  @param
-	 * */
-	void delete(Long id);
-	
 
-	
-	
+    /**
+     * ユーザー情報全件検索
+     * @param user 検索用リクエストデータ
+     * @return 検索結果
+     */
+    List<UserInfo> findAll();
+
+    /**
+     * ユーザー情報主キー検索
+     * @param id 主キー
+     * @return 検索結果
+     */
+    UserInfo findById(Long id);
+
+    /**
+     * ユーザー情報検索
+     * @param user 検索用リクエストデータ
+     * @return 検索結果
+     */
+    List<UserInfo> search(UserSearchRequest user);
+
+    /**
+     * ユーザー情報登録
+     * @param userRequest 登録用リクエストデータ
+     */
+    void save(UserAddRequest userRequest);
+
+    /**
+     * ユーザー情報更新
+     * @param userUpdateRequest 更新用リクエストデータ
+     */
+    void update(UserUpdateRequest userUpdateRequest);
+
+    /**
+     * ユーザー情報の論理削除
+     * @param id ID
+     */
+    void delete(Long id);
 }
