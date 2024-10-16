91e0285fd99928483aa9a9d678befb5df834ad55
uetak330 <uetak330@gmail.com>
2024-10-16 21:10:03 +0900

    ユーザー登録用のリクエストデータ格納クラス

---
+++ springSample/src/main/java/com/example/demo/dto/UserAddRequest.java
@@ -0,0 +1,26 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserAddRequest implements Serializable {
+	
+	@NotEmpty(message="名前を入力してください")
+	@Size(max = 100, message="名前は100文字以内で入力してください")
+	private String name;
+	
+	@Size(max = 255, message = "255文字以内で入力してください")
+	private String address;
+	
+	@Pattern(regexp = "0\\d{1,4}-\\d{1,4}-\\d{4}" ,message = "電話番号の形式で入力してください")
+	private String phone;
+	
+	
+	
+
+}
