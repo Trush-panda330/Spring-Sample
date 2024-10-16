c29b9a99c55c252376c6660cb233a2f4d27c15e2
uetak330 <uetak330@gmail.com>
2024-10-16 21:08:47 +0900

    rename User to UserInfo

--- springSample/src/main/java/com/example/demo/entity/UserInfo.java
+++ springSample/src/main/java/com/example/demo/entity/UserInfo.java
@@ -2,18 +2,17 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
-@Table(name = "user")
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,13 +25,10 @@ public class User {
 	
 	private String phone;
 	
-	@Column(name = "create_date")
 	private LocalDateTime createDate;
 	
-	@Column(name = "update_date")
 	private LocalDateTime updateDate;
 	
-	@Column(name = "delete_date")
 	private LocalDateTime deleteDate;
 	
 
