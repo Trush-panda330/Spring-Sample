34aafe14febbc03d30176f2ffce4331e34088661
uetak330 <uetak330@gmail.com>
2024-10-18 00:08:09 +0900

    Configクラスの定義

---
+++ SampleWeb/src/main/java/com/example/demo/config/BeanDefine.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeanDefine {
+	
+	/**
+	 * @Configurationとセットで@Beanを付与したメソッドはDIコンテナに登録され
+	 * DI注入(@RequiredArgsConstructor配下でprivate finalしてインスタンス化)
+	 * することができる*/
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
