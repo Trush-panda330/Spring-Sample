1bdadaf70180d199b86935fd015a80a4b7bff4db
uetak330 <uetak330@gmail.com>
2024-10-18 07:54:07 +0900

    message.propertiesに定義したメッセージを使用するように変更

--- SampleWeb/src/main/java/com/example/demo/controller/LoginController.java
+++ SampleWeb/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.context.MessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.constant.ErrorMessageConst;
 import com.example.demo.form.LoginForm;
 import com.example.demo.service.LoginService;
+import com.example.demo.util.AppUtill;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +27,9 @@ public class LoginController {
 	
 	/** PasswordEncoder*/
 	private final  PasswordEncoder passwordEncoder;
+	
+	/** メッセージソース*/
+	private final MessageSource messageSource;
 	
 	/**
 	 *  初期表示
@@ -59,10 +65,9 @@ public class LoginController {
 		// ユーザーとパスワードの組み合わせが合わない時
 		} else {
 			//TODO エラーメッセージはプロパティファイルで管理する
-			model.addAttribute("errorMsg", "ログインIDとパスワードの組み合わせが間違っています。");
+			var errorMsg = AppUtill.getMessage(messageSource,ErrorMessageConst.LOGIN_WRONG_INPUT);
+			model.addAttribute("errorMsg", errorMsg);
 			return "login";
 		}
-
 	}
-
 }
