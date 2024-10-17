95d5d3caf7d81608d8237089ddb2e2c99d9e6442
uetak330 <uetak330@gmail.com>
2024-10-18 08:43:16 +0900

    configファイルにDozerMapperについて追記ｘ

--- SampleWeb/src/main/java/com/example/demo/config/BeanDefine.java
+++ SampleWeb/src/main/java/com/example/demo/config/BeanDefine.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +17,11 @@ public class BeanDefine {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	Mapper mapper() {
+		return new DozerBeanMapper();
 	}
 
 }
