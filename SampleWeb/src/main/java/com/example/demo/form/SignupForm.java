1c84b1bc75195051a09bea31a569de1ad5f66544
uetak330 <uetak330@gmail.com>
2024-10-18 23:00:34 +0900

    バリデーションとJavadocコメントの追記

--- SampleWeb/src/main/java/com/example/demo/form/SignupForm.java
+++ SampleWeb/src/main/java/com/example/demo/form/SignupForm.java
@@ -1,5 +1,7 @@
 package com.example.demo.form;
 
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 /**
@@ -8,8 +10,12 @@ import lombok.Data;
 @Data
 public class SignupForm {
 	
+	/**ログインID*/
+	@Length(min = 8, max = 20)
 	private String loginId;
 	
+	/**パスワード*/
+	@Length(min = 8,max = 20)
 	private String password;
 
 }
