32e3f7e45b9baf79ae79ebecdf2206da5b8b9f34
uetak330 <uetak330@gmail.com>
2024-10-18 09:09:26 +0900

    新規登録の際に既に登録済みのIDだった場合に出力するメッセージを定義
    併せて定数としてコンストクラスにも定義

--- SampleWeb/src/main/java/com/example/demo/constant/ErrorMessageConst.java
+++ SampleWeb/src/main/java/com/example/demo/constant/ErrorMessageConst.java
@@ -7,4 +7,6 @@ public class ErrorMessageConst {
 	/**ログイン画面：入力内容誤り*/
 	public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
 
+	/**ユーザー登録画面：既に登録されているログインID*/
+	public static final String SIGNUP_EXISTED_LOGIN_ID = "signup.existedLoginId";
 }
