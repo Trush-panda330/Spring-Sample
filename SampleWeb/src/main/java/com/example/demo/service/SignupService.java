c35dfb40f0a1ca057080c7494d5c864f52bdb432
uetak330 <uetak330@gmail.com>
2024-10-18 17:44:19 +0900

    Optionalを使いformからidで検索しユーザが登録済みならisEmptyとし登録が重複しないようにサービスの実装を変更

--- SampleWeb/src/main/java/com/example/demo/service/SignupService.java
+++ SampleWeb/src/main/java/com/example/demo/service/SignupService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.dozer.Mapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -36,13 +38,24 @@ public class SignupService {
 	/**
 	 * ユーザー情報テーブル 新規登録
 	 * @param form 入力情報
-	 * @return 登録情報（ユーザー情報Entity)
+	 * @return 登録情報（ユーザー情報Entity)、既にユーザーIDで登録情報がある場合はEmpty
 	 * 
 	 */
-	public UserInfo resistUserInfo(SignupForm form) {
-//		var userInfo = new UserInfo();
-//		userInfo.setLoginId(form.getLoginId());
-//		userInfo.setPassword(form.getPassword());
+	public Optional <UserInfo> resistUserInfo(SignupForm form) {
+		//使う際に型が分かりにくいので変数名にOptとつけている
+		 var uesrInfoExistedOpt = repository.findById(form.getLoginId()); 
+		 if(uesrInfoExistedOpt.isPresent()) {//isPresentなので存在したらtrueが返る
+			 return Optional.empty();
+		 }
+			/* この処理はDozerMapperを使って実装
+			 * DozerMapperにより
+			 * インスタンスを生成し
+			 * フィールドをコピーする処理を
+			 * DozerMapperに任せられる
+			 * 
+			 *  var userInfo = new UserInfo();
+			 *  userInfo.setLoginId(form.getLoginId());
+			 *  userInfo.setPassword(form.getPassword());*/
 		
 		/* 第一引数の(form)情報を
 		 * 第二引数(UserInfo.class)コピーしてnewしたインスタンスを返す*/
@@ -51,7 +64,7 @@ public class SignupService {
 		var encodedPassword = passwordEncoder.encode(form.getPassword());
 		userInfo.setPassword(encodedPassword);
 		
-		return repository.save(userInfo);
+		return Optional.of(repository.save(userInfo));
 	}
 
 }
