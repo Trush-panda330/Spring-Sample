a9187cebbfe815b79b660b4801761ec8d86dea47
uetak330 <uetak330@gmail.com>
2024-10-25 13:35:43 +0900

    コメント追記

--- SampleWeb/src/main/java/com/example/demo/config/BeanDefine.java
+++ SampleWeb/src/main/java/com/example/demo/config/BeanDefine.java
@@ -4,20 +4,22 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanDefine {
 
 	/*
 	 * @Configurationとセットで@Beanを付与したメソッドはDIコンテナに登録され
-	 * DI注入(@RequiredArgsConstructorを付けてprivate finalしてインスタンス化)
-	 * することができる*/
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	 * DI注入(@RequiredArgsConstructorを付けてprivate finalしてインスタンス化)することができる。
+	 * この設定によりSpringSecurityは認証チェックでパスワードの比較を行う際、
+	 * 「登録されているパスワードはどのようなエンコードになっているのか」というのを
+	 * PasswordEncoderの＠Bean内容で判断する。
+	 * ※＠Beanがなければエンコードなし（ただの文字列と見なす）*/
+	//TODO　後からコメ外す
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 	
 	@Bean
 	Mapper mapper() {
