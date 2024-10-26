3fc20f7fe01a476d425528d9bd4686fa31800120
uetak330 <uetak330@gmail.com>
2024-10-26 11:53:46 +0900

    UserInfoエンティティの修正
    ログイン失敗回数をリセットするメソッドと
    アカウントロックするメソッドを作成
    
    どちらもコンストラクタを使用していて
    UserDetailServiceの実装クラスで使用している

--- SampleWeb/src/main/java/com/example/demo/entity/UserInfo.java
+++ SampleWeb/src/main/java/com/example/demo/entity/UserInfo.java
@@ -1,10 +1,14 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * ユーザー情報テーブル Entity
@@ -12,6 +16,8 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name="user_info")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo {
 	
 	/** ログインID*/
@@ -21,5 +27,35 @@ public class UserInfo {
 	
 	/**パスワード*/
 	private String password;
-
+	
+	/** ログイン失敗回数*/
+	@Column(name = "login_failure_count")
+	private int loginFailureCount = 0;
+	
+	@Column(name = "account_locked_time")
+	private LocalDateTime accountLockedTime;
+	
+	@Column(name="is_disabled")
+	private boolean isDisabled;
+	
+	/**
+	 * ログイン失敗回数をインクリメントする
+	 * @return ログイン失敗回数がインクリメントされたUserInfo
+	 */
+	public UserInfo incrementLoginFailureCount() {
+		return new UserInfo(loginId, password, ++loginFailureCount, accountLockedTime, isDisabled);
+	}
+	
+	/**
+	 * ログイン失敗情報をリセットする
+	 * 
+	 * @return ログイン失敗情報がリセットされたUserInfo
+	 */
+	public UserInfo resetLoginFailureInfo() {
+		return new UserInfo(loginId, password, 0, null, isDisabled);
+	}
+	
+	public UserInfo updateAccountLocked() {
+		return new UserInfo(loginId, password, 0, LocalDateTime.now(), isDisabled);
+	}
 }
