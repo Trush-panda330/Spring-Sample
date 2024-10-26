c0be48e6c7e2c4717718b51acd981222045a0b97
uetak330 <uetak330@gmail.com>
2024-10-26 11:42:52 +0900

    Providerの定義

--- SampleWeb/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ SampleWeb/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,19 +1,36 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
 // 必要なライブラリをインポート
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.constant.UrlConst;
+
+import lombok.RequiredArgsConstructor;
 
 // Webセキュリティを有効にするアノテーション
 @EnableWebSecurity
 // このクラスが設定クラスであることを示すアノテーション
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+	
+	/** パスワードエンコーダー*/
+	private final PasswordEncoder passwordEncoder;
+	
+	/** ユーザー情報取得Ser-vice*/
+	private final UserDetailsService userDetailsService;
+	
+	/** メッセージ取得*/
+	private final MessageSource messageSource;
 
 	/** ユーザー名name属性*/
 	private final String USERNAME_PARAMETER = "loginId";
@@ -38,5 +55,19 @@ public class WebSecurityConfig {
 
 		// HttpSecurityオブジェクトをビルドして返す
 		return http.build();
+	}
+	
+	/**
+	 * Provider定義
+	 * 
+	 * @return カスタマイズProvider情報
+	 */
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setMessageSource(messageSource);
+		return provider;
 	}
 }
